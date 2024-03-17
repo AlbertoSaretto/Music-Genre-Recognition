3ee9fe9113f44697f447465dd7ca4eb31be28d01
diegobonato <diegobonato99@gmail.com>
2024-03-17 16:57:42 +0100

    changing kernels

--- CNN1D/cnn1d.py
+++ CNN1D/cnn1d.py
@@ -313,6 +313,102 @@ class NNET1D_BN_DropOut(nn.Module):
         x = self.fc(x)
         return x 
 
+# Changing Kernels
+class NNET1D_K(nn.Module):
+        
+    def __init__(self):
+        super(NNET1D_K, self).__init__()
+        
+        
+        self.c1 = nn.Sequential(
+            nn.Conv1d(in_channels=1, out_channels=64, kernel_size=3, stride=8, padding=64),
+            nn.BatchNorm1d(64),
+            nn.ReLU(inplace = True),
+            nn.MaxPool1d(kernel_size=4, stride=4),
+          
+        )
+        
+
+        self.c2 = nn.Sequential(
+            nn.Conv1d(in_channels=64, out_channels=128, kernel_size=8, stride=2, padding=16),
+            nn.BatchNorm1d(128),
+            nn.ReLU(inplace = True),
+            nn.MaxPool1d(kernel_size=2, stride=2),
+          
+        )
+
+        self.c3 = nn.Sequential(
+            nn.Conv1d(in_channels=128, out_channels=256, kernel_size=16, stride=2, padding=8),
+            nn.BatchNorm1d(256),
+            nn.ReLU(inplace = True),
+            nn.MaxPool1d(kernel_size=2, stride=2),
+         
+        )
+        
+        #Trying to add 4th convolutional block
+        self.c4 = nn.Sequential(
+            nn.Conv1d(in_channels=256, out_channels=512, kernel_size=8,stride=2, padding=4),
+            nn.BatchNorm1d(512),
+            nn.ReLU(inplace = True),
+           
+        )
+        
+
+        self.fc = nn.Sequential(
+            nn.Linear(4096, 1024), 
+            nn.ReLU(inplace = True),
+
+            nn.Linear(1024, 512), 
+            nn.ReLU(inplace = True),
+
+            nn.Linear(512, 128),
+            nn.ReLU(inplace = True),
+
+           # nn.Linear(128,128),
+          #  nn.ReLU(inplace = True),
+            
+            nn.Linear(128, 64),
+            nn.ReLU(inplace = True),
+           
+            nn.Linear(64, 8),
+            nn.Softmax(dim=1)
+        )
+
+
+        self.apply(self._init_weights)
+
+    def _init_weights(self, module):
+        # Initialize only self.classifer weights
+        # We need the weights of the trained CNNs
+        if isinstance(module, nn.Linear):
+            nn.init.xavier_uniform_(module.weight)
+            nn.init.constant_(module.bias, 0.0)
+        
+
+    def forward(self, x):
+        
+        c1 = self.c1(x)
+            
+        c2 = self.c2(c1)
+       
+        c3 = self.c3(c2)
+        
+        c4 = self.c4(c3)
+       
+        max_pool = F.max_pool1d(c4, kernel_size=64)
+        avg_pool = F.avg_pool1d(c4, kernel_size=64)
+
+        #Concatenate max and average pooling
+        x = torch.cat([max_pool, avg_pool], dim = 1) 
+
+        
+        # x dimensions are [batch_size, channels, length, width]
+        # All dimensions are flattened except batch_size  
+        x = torch.flatten(x, start_dim=1)
+
+        x = self.fc(x)
+        return x 
+
 if __name__ == "__main__":
 
     # Data augmentation
@@ -335,7 +431,7 @@ if __name__ == "__main__":
     Sembra che i dati siano già parzialmente normalizzati
     """
 
-    model_net = LitNet(NNET1D_BN())
+    model_net = LitNet(NNET1D_K())
     lr=1e-4
     #optimizer = torch.optim.SGD(model_net.parameters(), lr=lr,momentum=0.2)
 
@@ -344,7 +440,7 @@ if __name__ == "__main__":
                 net_type='1D',
                 transforms=None,
                 PATH_DATA="../data/",
-                batch_size=32,
+                batch_size=16,
                 fast_dev_run=False,
                 optimizer=None, # None --> Adam,
                 lr=lr,
